12d20fd99c9ff2b640c9be22fcc781aba95f771d
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-03 00:33:11 +0900

    fix:connect MongoDb database

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import{ MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            useFactory:() =>({
+                uri:process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : process.env.MONGODB_URI_DEV,
+            })
+        })
+    ],
+    exports: [MongooseModule],
+})
+export class DatabaseModule {
+    constructor(@InjectConnection()private readonly connection:Connection) {
+        if (connection.readyState === 1){
+            console.log(`MongoDb is connected into ${process.env.NODE_ENV === 'production' ? 'Production' : 'Development'} mode`);
+        }else{
+            console.log('Db is not connected!')
+        }
+    }
+}
+
