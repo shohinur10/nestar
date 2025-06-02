d17d15a9e10d343b6295fcdd00c5f2d9c3df0c7d
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-02 14:34:29 +0900

    feat: build project stanadrd

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = 'Something went wrong',
+  NO_DATA_FOUND = 'No data found',
+  CREATE_FAILED = 'Create failed',
+  UPDATED_FAILED = 'Updated failed',
+  REMOVE_FAILED = 'Remove failed',
+  UPLOAD_FAILED = 'Upload failed',
+  BAD_REQUEST = 'Bad request',
+
+  NO_MEMBER_NICK = 'No member nick found',
+  BLOCKED_USER = 'You have been blocked',
+  WRONG_PASSWORD = 'Wrong password, Try again',
+  NOT_AUTHENTICATED = ' You Are Not authenticated, please login first',
+  TOKEN_NOT_EXISTED = 'Bearer Token is not provided',
+  ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles',
+  NOT_ALLOWED_REQUEST = 'Not allowed request',
+  PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg or png images',
+  SELF_SUBSCRIPTION_DENIED = 'Self subscription denied',
+}
