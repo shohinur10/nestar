05b0c294c31ae87bdffa078224653f9c68aa3dea
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-05 18:11:29 +0900

    feat: create member sche,ma model

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import MemberSchema from '../../schemas/Member.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]) // Assuming you have a MemberSchema defined
+    
+  ],
   providers: [MemberResolver, MemberService]
 })
 export class MemberModule {}
