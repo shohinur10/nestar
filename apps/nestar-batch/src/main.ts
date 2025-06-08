32484cd4dc4b2fe8760f77571a514cd34d4334e4
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-08 11:55:10 +0900

    feat: standard integration opf lagging interceptors:

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { NestarBatchModule } from './nestar-batch.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from 'apps/nestar-api/src/libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
+
+//this is about how much time it takes to run and get answer from service
