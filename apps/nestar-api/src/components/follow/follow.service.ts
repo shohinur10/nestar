2f866abbcecf1390af1c586a521bc4b9a3f80b52
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-24 16:02:30 +0900

    feat: integration of lookupAuthMemberFollowed complex query business logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -5,7 +5,7 @@ import { Follower, Followers, Following, Followings } from '../../libs/dto/follo
 import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData} from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData} from '../../libs/config';
 import { T } from '../../libs/types/common';
 
 @Injectable()
@@ -79,10 +79,10 @@ export class FollowService {
 							// meLiked
 							lookupAuthMemberLiked(memberId, '$followingId'),
 							// meFollowed
-							// lookupAuthMemberFollowed({
-							// 	followerId: memberId,
-							// 	followingId: '$followingId',
-							// }),
+							lookupAuthMemberFollowed({
+								followerId: memberId,
+								followingId: '$followingId',
+							}),
 							lookupFollowingData,
 							{ $unwind: '$followingData' },
 						],
@@ -114,10 +114,10 @@ export class FollowService {
 							// meLiked
 							lookupAuthMemberLiked(memberId, '$followerId'),
 							// meFollowed
-							// lookupAuthMemberFollowed({
-							// 	followerId: memberId,
-							// 	followingId: '$followerId',
-							// }),
+							lookupAuthMemberFollowed({
+								followerId: memberId,
+								followingId: '$followerId',
+							}),
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
 						],
