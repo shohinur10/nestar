a18855a0b3af71f96e918bbcfdb9ce4416f065ad
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-27 16:45:07 +0900

    fix: Socket business logic

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,24 +1,30 @@
 import { Module } from '@nestjs/common';
-import{ MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
 @Module({
-    imports: [
-        MongooseModule.forRootAsync({
-            useFactory:() =>({
-                uri:process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : process.env.MONGODB_URI_DEV,
-            })
-        })
-    ],
-    exports: [MongooseModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri:
+          process.env.NODE_ENV === 'production'
+            ? process.env.MONGO_PROD // ✅ mos nom
+            : process.env.MONGO_DEV,  // ✅ mos nom
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
 })
 export class DatabaseModule {
-    constructor(@InjectConnection()private readonly connection:Connection) {
-        if (connection.readyState === 1){
-            console.log(`MongoDb is connected into ${process.env.NODE_ENV === 'production' ? 'Production' : 'Development'} mode`);
-        }else{
-            console.log('Db is not connected!')
-        }
+  constructor(@InjectConnection() private readonly connection: Connection) {
+    if (connection.readyState === 1) {
+      console.log(
+        `✅ MongoDB is connected into ${
+          process.env.NODE_ENV === 'production' ? 'Production' : 'Development'
+        } mode`,
+      );
+    } else {
+      console.log('❌ DB is not connected!');
     }
+  }
 }
-
