98adeff183cae7e3587773e438460cd9a247ea86
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-06-27 15:32:57 +0900

    feat: batchRollback,batchProperties and batchagentc jobSchedulers logic

--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -4,10 +4,19 @@ import { ConfigModule } from '@nestjs/config';
 import { BatchController } from './batch.controller';
 import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import PropertySchema from 'apps/nestar-api/src/schemas/Property.model';
+import MemberSchema from 'apps/nestar-api/src/schemas/Member.model';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
-  controllers: [BatchController],
-  providers: [BatchService],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
+	controllers: [BatchController],
+	providers: [BatchService],
 })
 export class BatchModule {}
